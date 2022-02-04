6399f1ba650f5ece24d495d23231ab5332c70b98
Andrii Yaremenko <iriver87@gmail.com>
2022-02-04 18:59:35 +0000

    Finallyze setup file and tume cli input

--- setup.py
+++ setup.py
@@ -4,14 +4,22 @@ from setuptools import find_packages, setup
 with open('README.md', 'r') as f:
         long_description = f.read()
 
-        setup(
-            name = 'pgbackup',
-            version = '0.1.0',
-            author = 'Andrii Yaremenko'
-            author_email = 'iriver87@example.com',
-            description = 'Utility to make backup of the PostgreSQL database',
-            long_description = long_description,
-            long_description_content_type = 'text/markdown',
-            url = 'https://github.com/du4ok/pgbackup',
-            packages = find_packages('src')
-             )
+setup(
+    name = 'pgbackup',
+    version = '0.1.0',
+    author = 'Andrii Yaremenko',
+    author_email = 'iriver87@example.com',
+    description = 'Utility to make backup of the PostgreSQL database',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/du4ok/pgbackup',
+    packages = find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main'
+        ],
+    }
+)
